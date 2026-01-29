948a0aa0e2858e799665493a570f809ff841cac5
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-30 00:55:23 +0530

    Add footer

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,7 +1,16 @@
+import { RiGithubFill, RiLinkedinFill } from "@remixicon/react";
+
 export function Footer(){
     return(
-        <div>
-            
+        <div className="bg-zinc-100 font-bold font-used px-20 flex justify-between items-center h-20">
+             <div className=" font-black text-3xl">
+                Todo<span className="inline-block bg-blue-500 rounded-full h-2 w-2 "></span>Board
+            </div>
+        <p className="font-semibold">Made by Jatin</p>
+        <div className="flex gap-5">
+        <RiLinkedinFill />
+        <RiGithubFill />
+        </div>
         </div>
     )
 }

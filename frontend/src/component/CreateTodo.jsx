f54016df3c0318ba7e8c46d434a6302bce6de545
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-30 10:12:38 +0530

    Add color to text

--- frontend/src/component/CreateTodo.jsx
+++ frontend/src/component/CreateTodo.jsx
@@ -1,52 +1,46 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 // start with capital letter CreateTodo
 export function CreateTodo({setTodos}) {
    // react query
    const [title, setTitle] = useState("");
    const [description, setDescription] = useState("");
-   //  const [completed, setCompleted] = useState("");
-   return <div>
 
-      <input id="title" style={{
-         padding: 22,
-         margin: 20
-
-      }} type="text" placeholder="title" onChange={function (e) {
+   return <div className="flex  font-used gap-5 mt-10 justify-center items-center ">
+      <div>
+      <p className="font-black text-2xl">Title</p>
+      <input id="title" className="px-2 border h-8" type="text" placeholder="Title" onChange={function (e) {
          const value = e.target.value;
          setTitle(e.target.value);
       }} ></input> <br />
+      </div>
 
-      <input id="desc" style={{
-         padding: 22,
-         margin: 20
-      }} type="text" placeholder="description" onChange={function (e) {
+      <div>
+      <p className="font-black text-2xl">Description</p>
+      <textarea id="desc" type="text" className="px-2 h-8 mt-1 border resize-none" placeholder="Description" onChange={function (e) {
          const value = e.target.value;
          setDescription(e.target.value);
-      }} />  <br />
+      }} /> 
+      </div>
 
-      <button style={{
-         padding: 22,
-         margin: 20
-
-      }} onClick={() => {
+      <button className="bg-[#0083ff] text-white    px-5 font-bold py-3" onClick={() => {
          // axois
          fetch("http://localhost:3000/todo", {
             method: "POST",
-            body: JSON.stringify({
-               title: title,
-               description: description,
-               // completed: completed
-            }),
+            body: JSON.stringify({ title, description }),
             headers: {
-               "content-type": "application/json"
+              "Content-Type": "application/json",
+              "Authorization": localStorage.getItem("token")
             }
-         })
-            .then(async function (res) {
-               const json = await res.json();
-               setTodos(prev=>[...prev,json])
-               alert("Todo added");
-            })
+          })
+          .then(async (res) => {
+            const json = await res.json();
+            setTodos(prev => [...prev, json]);
+            toast.success("Todo added");
+            setTitle("");
+            setDescription("");
+          });
       }}>Add a Todo</button>
 
    </div>

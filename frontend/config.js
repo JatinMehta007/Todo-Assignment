f5c1fc7ef8cc908e9995253a7e984468ba82c155
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-30 14:55:31 +0530

    add backend url

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL ="http://localhost:3000"
+export const BACKEND_URL ="https://todo-backend-ten-beige.vercel.app"
+
+// add your backend url if you clone the repo http://localhost:3000

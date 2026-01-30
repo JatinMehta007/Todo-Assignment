21ce4adf232f08ee1f0575e0cc5b9c59eefa0793
Jatin_Mehta <mehtajatin045@gmail.com>
2026-01-30 10:14:09 +0530

    add if todos are not created then create

--- frontend/src/component/Todos.jsx
+++ frontend/src/component/Todos.jsx
@@ -1,3 +1,7 @@
+import { RiDeleteBin6Fill } from "@remixicon/react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 export function Todos({ todos, setTodos }) {
 
       const markCompleted = async (id) => {
@@ -9,7 +13,6 @@ export function Todos({ todos, setTodos }) {
           body: JSON.stringify({ id })
         });
     
-        // Update UI
         setTodos(prev =>
           prev.map(todo =>
             todo._id === id ? { ...todo, completed: true } : todo
@@ -17,24 +20,59 @@ export function Todos({ todos, setTodos }) {
         );
       };
     
+      const Deleted = async (id)=>{
+         try{
+          const res = await axios.delete(`http://localhost:3000/todo/${id}`,{
+            headers : {
+              Authorization : localStorage.getItem("token")
+            }
+          });
+
+          setTodos(prev => prev.filter(todo=>todo._id !== id));
+          toast.success("Deleted Successfully");
+         } catch(err){
+          toast.error("Delete Failed");
+          console.error(err);
+         }
+      }
+
       return (
-        <div>
-          {todos.map((todo) => (
-            <div key={todo._id} className="text-used">
+        <div className="flex flex-col mt-24 font-used justify-center items-center gap-10">
+          { todos.length === 0 ? ( 
+            <div className="text-center mt-20">
+        <h1 className="text-3xl font-black text-zinc-700">
+          No Todos Yet
+        </h1>
+        <p className="text-zinc-500 mt-2">
+          Create your first todo above 🚀
+        </p>
+      </div>
+          )
+          : ( todos.map((todo) => (
+              <div key={todo._id} className="text-used border   p-2 w-[70%] grid grid-flow-col">
+              <div>
               <h1 className="text-black text-2xl capitalize font-black">
-                {todo.title}
+              {todo.title}
               </h1>
-    
+              
               <h2>{todo.description}</h2>
-    
+              </div>
+              <div className="flex justify-end gap-5 items-center">
               <button
-                onClick={() => markCompleted(todo._id)}
-                className="bg-blue-500 text-white px-4 py-1 rounded"
+              onClick={() => markCompleted(todo._id)}
+              className=" border border-dashed border-zinc-500 text-black px-4 py-1 font-bold rounded"
               >
-                {todo.completed ? "Completed" : "Mark as completed"}
+              {todo.completed ? "Completed" : "Mark as completed"}
               </button>
-            </div>
-          ))}
+              <button onClick={()=>Deleted(todo._id)} className="">
+              <RiDeleteBin6Fill></RiDeleteBin6Fill>
+              </button>
+              
+              </div>
+              </div>
+            )))
+          
+        }
         </div>
       );
     }
